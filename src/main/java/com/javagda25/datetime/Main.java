84e34aa6d6536446a4148ff2fc0f99715172b495
Paweł Recław <pawel.reclaw@gmail.com>
2019-06-06 18:04:14 +0200

    Date time and periods.

--- src/main/java/com/javagda25/datetime/Main.java
+++ src/main/java/com/javagda25/datetime/Main.java
@@ -1,7 +1,37 @@
 package com.javagda25.datetime;
 
+import java.time.*;
+import java.time.temporal.TemporalUnit;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
+        // źle!!!!!!!!
 
+//        Date date = new Date();
+//        System.out.println(date);
+//
+//        long milisecondsTill1970 = date.getTime();
+
+        LocalDate data = LocalDate.now(); // obecna data!
+        // 24.06.2019
+        // 00:00:00 24.06.2019
+        LocalDateTime dataICzasPoczatekDnia = data.atStartOfDay();
+        // zamiast:
+        LocalDateTime nowaDataICzas1 = LocalDateTime.of(data.getYear(), data.getMonth(), data.getDayOfMonth(), 0, 0);
+        LocalDateTime nowaDataICzas2Grunwald = LocalDateTime.of(1410, 7, 15, 15, 0);
+
+        LocalTime czas = LocalTime.now(); // obecny czas!
+        LocalDateTime dataICzas = LocalDateTime.now();
+
+        // Klasy reprezentujące odcinki czasu:
+        Period okres = Period.between(nowaDataICzas2Grunwald.toLocalDate(),
+                dataICzasPoczatekDnia.toLocalDate());
+
+        System.out.println("Minelo: " + okres.getDays() + " " + okres.getMonths() + " " + okres.getYears());
+        System.out.println(okres);
+
+        Duration duration = Duration.between(nowaDataICzas2Grunwald, dataICzasPoczatekDnia);
+        System.out.println(duration.getSeconds());
     }
 }
